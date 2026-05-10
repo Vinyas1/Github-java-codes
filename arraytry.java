0ee46408a47884661bdc6c6b172aeaf40d1e7ea8
vinyas1 <vinyask193@gmail.com>
2026-05-11 00:54:56 +0530

    java-prog-pc

---
+++ arraytry.java
@@ -0,0 +1,17 @@
+package javex;
+import java.util.*;
+public class arraytry {
+
+	public static void main(String[] args) {
+
+int [] stack = null ;
+stack[1] = 28;
+stack[2] = 23;
+stack[3] = 28;
+stack[9] = 23;
+stack[14] = 28;
+stack[23] = 23;
+System.out.println(stack);
+	}
+
+}
