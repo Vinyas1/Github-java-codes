511d969e39e7c427c658e70bab54f3cdb5b75d4b
vinyas1 <vinyask193@gmail.com>
2026-05-11 00:55:43 +0530

    java-prog-pc

---
+++ pc-1/Typeconv.java
@@ -0,0 +1,17 @@
+package jav1;
+
+public class Typeconv {
+	public static void main(String args[]) {
+		int a=257;
+		byte b;
+		float g =323.567f;
+		System.out.println("conversion of int to byte");
+		b=(byte)a;
+		System.out.println("conversion of float to int ");
+		g=(int)g;
+		System.out.println("conversion of int to float ");
+		a=(float)a;
+		
+	}
+
+}
