9b9505eec5e0bfff0b4f9d340b35c604a93c076e
vinyas1 <vinyask193@gmail.com>
2026-05-11 00:55:36 +0530

    java-prog-pc

---
+++ pc-1/Boolean.java
@@ -0,0 +1,14 @@
+package jav1;
+
+public class Boolean {
+    public static void main(String[] args) {
+      
+        int a = 10;
+        int b = 20;
+        
+        
+    
+            System.out.println("a>b  is "+(a>b));
+     
+    }
+}
