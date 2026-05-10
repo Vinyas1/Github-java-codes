216f06a26ac44c1c1f55e712391433a78f2f59c7
vinyas1 <vinyask193@gmail.com>
2026-05-11 00:55:34 +0530

    java-prog-pc

---
+++ pc-1/Areaofc.java
@@ -0,0 +1,13 @@
+package jav1;
+
+public class Areaofc {
+	public static void main (String args[]) {
+		float r ,pi ,a;
+		r=25.5f;
+        pi=3.14f;
+		a=pi*r*r;
+		System.out.println("the area of the circle is =" +a);
+	}
+	
+
+}
