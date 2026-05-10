c480ca15951bf638f82e841dac96102534750b0e
vinyas1 <vinyask193@gmail.com>
2026-05-11 00:55:35 +0530

    java-prog-pc

---
+++ pc-1/arrayfor.java
@@ -0,0 +1,19 @@
+package jav1;
+
+public class arrayfor {
+	public static void main(String args[] ){
+	int n[];
+	n=new int [5];
+	int k=0;
+	for (int i=0;i<5;i++) {
+		n[i]=k;
+		k=k+5;
+		
+	}
+	for (int i=0;i<5;i++) {
+	
+	System.out.println(+n[i]);
+	}
+	}
+
+}
