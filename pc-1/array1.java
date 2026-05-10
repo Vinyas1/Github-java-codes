77aef30052b038a1f461b7204fad251d066ef834
vinyas1 <vinyask193@gmail.com>
2026-05-11 00:55:34 +0530

    java-prog-pc

---
+++ pc-1/array1.java
@@ -0,0 +1,13 @@
+package jav1;
+
+import java.util.Arrays;
+
+public class array1 {
+    public static void main(String[] args) {
+      
+        int[] a = {2, 4, 5, 6, 7, 6, 8, 9};
+      
+       
+        System.out.println("The array elements are: " + Arrays.toString(a));
+    }
+}
