1da3677bf1be3b4685189feb92b33d72add8cca6
vinyas1 <vinyask193@gmail.com>
2026-05-11 00:55:30 +0530

    java-prog-pc

---
+++ jenkins3.java
@@ -0,0 +1,23 @@
+package javex;
+import java.util.*;
+
+public class jenkins3 {
+
+	public static void main(String[] args) {
+
+ArrayDeque<Integer> ad  =new ArrayDeque<>();
+ad.push(554);
+ad.push(24);
+ad.push(5421);
+ad.push(972);
+ad.push(4);
+
+
+
+
+while(!ad.isEmpty()) {
+	System.out.println(ad.pop());  
+}
+	}
+
+}
