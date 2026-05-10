34114c05307971986a96373efe20b51635109fbd
vinyas1 <vinyask193@gmail.com>
2026-05-11 00:55:43 +0530

    java-prog-pc

---
+++ pc-1/Two9.java
@@ -0,0 +1,17 @@
+package jav1;
+
+public class Two9 {
+public static void main (String args[]) {
+ int days;
+ int seconds;
+ long lightspeed;
+ long distance;
+ lightspeed =186000;
+ days =1000;
+ seconds =days*24*60*60;
+ distance =lightspeed * seconds;
+ System.out.println("the total time taken is "+seconds);
+ System.out.println("the total days taken is " +days);
+ System.out.println("speed ww it was travelling " +lightspeed );
+}
+}
