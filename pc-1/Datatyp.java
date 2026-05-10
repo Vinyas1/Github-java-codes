7265b601ce6c5c8f2e67201cf1deb6300626b099
vinyas1 <vinyask193@gmail.com>
2026-05-11 00:55:37 +0530

    java-prog-pc

---
+++ pc-1/Datatyp.java
@@ -0,0 +1,15 @@
+package jav1;
+
+public class Datatyp {
+	public static void main (String args[]) {
+		int a=0b1010;
+		int b=123_456_789;
+		int c=0b101001010001010;
+		float d=0x12.2P2f;
+		System.out.println("value of a is " +a);
+		System.out.println("value of b is " +b);
+		System.out.println("value of c is " +c);
+		System.out.println("value of d is " +d);
+	}
+
+}
