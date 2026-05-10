99ea89564a17c947e38e838bddca2aded51a246f
vinyas1 <vinyask193@gmail.com>
2026-05-11 00:55:35 +0530

    java-prog-pc

---
+++ pc-1/Asci.java
@@ -0,0 +1,12 @@
+package jav1;
+
+public class Asci {
+	public static void main(String args[]) {
+		char ch1,ch2;
+		ch1=88;
+	ch2=109;
+		System.out.println("the ch1 stands for " +ch1);
+		System.out.println("ch2 stands for "+ch2);
+	}
+
+}
